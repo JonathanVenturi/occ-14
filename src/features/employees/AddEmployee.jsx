e42bfc0ec2e582a7929c730a4a5a88ce2460cf60
Jonathan Venturi <jonathan.venturi@gmail.com>
2025-03-31 12:39:32 +0200

    Improved layout for new employee form

--- src/features/employees/AddEmployee.jsx
+++ src/features/employees/AddEmployee.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { Container, Form, Button, Modal } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import { Form, Button, Modal, Row, Col } from 'react-bootstrap'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.min.css'
 import { SimpleSelect } from '@jonathanventuri/react-simpleselect'
@@ -8,9 +9,8 @@ import { addEmployee } from './employeesSlice'
 import { US_STATES } from './helpers/states'
 import { DEPARTMENTS } from './helpers/departments'
 
-export function AddEmployee({ currentEmployee }) {
+export function AddEmployee() {
   const dispatch = useDispatch()
-
   const newEmployee = {
     firstName: '',
     lastName: '',
@@ -22,153 +22,182 @@ export function AddEmployee({ currentEmployee }) {
     zipAddress: '',
     companyDepartment: ''
   }
-
-  const [employee, setEmployee] = useState(
-    currentEmployee ? currentEmployee : newEmployee
-  )
-
+  const [employee, setEmployee] = useState(newEmployee)
   const [modal, setModal] = useState(false)
+  const navigate = useNavigate()
 
   return (
     <>
-      <Container fluid='sm' as='main'>
-        <Form onSubmit={handleFormSubmit} className='col-lg-6 offset-lg-3'>
+      <main>
+        <Form id='addEmployeeForm' onSubmit={handleFormSubmit}>
           <h2>Create Employee</h2>
-          <Form.Group controlId='firstName' className='mb-3'>
-            <Form.Label>First Name</Form.Label>
-            <Form.Control
-              type='text'
-              value={employee.firstName}
-              onChange={handleFirstNameChange}
-            />
-          </Form.Group>
-          <Form.Group controlId='lastName' className='mb-3'>
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              type='text'
-              value={employee.lastName}
-              onChange={handleLastNameChange}
-            />
-          </Form.Group>
-          <Form.Group controlId='birthDate' className='mb-3'>
-            <Form.Label>Birthdate</Form.Label>
-            <DatePicker
-              placeholderText='Click to select a date'
-              dateFormat='yyyy/MM/dd'
-              autoComplete='off'
-              showMonthDropdown
-              showYearDropdown
-              scrollableYearDropdown
-              yearDropdownItemNumber={50}
-              maxDate={new Date().setFullYear(new Date().getFullYear() - 14)}
-              popperPlacement='bottom-start'
-              showPopperArrow={false}
-              showIcon
-              calendarIconClassName='end-0'
-              wrapperClassName='container g-0'
-              customInput={<Form.Control />}
-              selected={employee.birthDate}
-              onChange={(date) =>
-                setEmployee({
-                  ...employee,
-                  birthDate: date.getTime()
-                })
-              }
-            />
-          </Form.Group>
-          <Form.Group controlId='startDate' className='mb-3'>
-            <Form.Label>Start Date</Form.Label>
-            <DatePicker
-              placeholderText='Click to select a date'
-              dateFormat='yyyy/MM/dd'
-              autoComplete='off'
-              showMonthDropdown
-              showYearDropdown
-              todayButton={<Button>Today</Button>}
-              popperPlacement='bottom-start'
-              showPopperArrow={false}
-              showIcon
-              calendarIconClassName='end-0'
-              wrapperClassName='container g-0'
-              customInput={<Form.Control />}
-              selected={employee.startDate}
-              onChange={(date) =>
-                setEmployee({
-                  ...employee,
-                  startDate: date.getTime()
-                })
-              }
-            />
-          </Form.Group>
+          <Row>
+            <Form.Group controlId='firstName' className='mb-3'>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type='text'
+                value={employee.firstName}
+                onChange={handleFirstNameChange}
+              />
+            </Form.Group>
+            <Form.Group controlId='lastName' className='mb-3'>
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type='text'
+                value={employee.lastName}
+                onChange={handleLastNameChange}
+              />
+            </Form.Group>
+            <Form.Group controlId='birthDate' className='mb-3'>
+              <Form.Label>Birthdate</Form.Label>
+              <DatePicker
+                placeholderText='Click to select a date'
+                dateFormat='yyyy/MM/dd'
+                autoComplete='off'
+                showMonthDropdown
+                showYearDropdown
+                scrollableYearDropdown
+                yearDropdownItemNumber={50}
+                maxDate={new Date().setFullYear(new Date().getFullYear() - 14)}
+                popperPlacement='bottom-start'
+                showPopperArrow={false}
+                showIcon
+                calendarIconClassName='end-0'
+                wrapperClassName='container g-0'
+                customInput={<Form.Control />}
+                selected={employee.birthDate}
+                onChange={(date) =>
+                  setEmployee({
+                    ...employee,
+                    birthDate: date.getTime()
+                  })
+                }
+              />
+            </Form.Group>
+          </Row>
+
           <fieldset className='form-group border px-3 mb-3'>
             <legend className='float-none w-auto p-2'>Address</legend>
-            <Form.Group controlId='streetAddress' className='mb-3'>
-              <Form.Label>Street</Form.Label>
-              <Form.Control
-                type='text'
-                value={employee.streetAddress}
-                onChange={handleStreetAddressChange}
-              />
-            </Form.Group>
-            <Form.Group controlId='cityAddress' className='mb-3'>
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                type='text'
-                value={employee.cityAddress}
-                onChange={handleCityAddressChange}
-              />
-            </Form.Group>
-            <Form.Group controlId='stateAddress' className='mb-3'>
-              <Form.Label>State</Form.Label>
+            <Row>
+              <Form.Group controlId='streetAddress' className='mb-3'>
+                <Form.Label>Street</Form.Label>
+                <Form.Control
+                  type='text'
+                  value={employee.streetAddress}
+                  onChange={handleStreetAddressChange}
+                />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col} controlId='cityAddress' className='mb-3'>
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  type='text'
+                  value={employee.cityAddress}
+                  onChange={handleCityAddressChange}
+                />
+              </Form.Group>
+              <Form.Group as={Col} controlId='stateAddress' className='mb-3'>
+                <Form.Label>State</Form.Label>
+                <SimpleSelect
+                  as={Form.Select}
+                  aria-label='State'
+                  placeholder='Select a state'
+                  value={employee.stateAddress}
+                  onChange={handleStateAddressChange}
+                  options={US_STATES}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId='zipAddress' className='mb-3'>
+                <Form.Label>Zip Code</Form.Label>
+                <Form.Control
+                  type='number'
+                  value={employee.zipAddress}
+                  onChange={handleZipAddressChange}
+                />
+              </Form.Group>
+            </Row>
+          </fieldset>
+          <Row as='fieldset' className='form-group border px-3 mx-0 mb-3'>
+            <legend className='float-none w-auto p-2'>
+              Company Credentials
+            </legend>
+            <Form.Group as={Col} controlId='companyDepartment' className='mb-3'>
+              <Form.Label>Department</Form.Label>
               <SimpleSelect
                 as={Form.Select}
-                aria-label='State'
-                placeholder='Select a state'
-                value={employee.stateAddress}
-                onChange={handleStateAddressChange}
-                options={US_STATES}
+                aria-label='Company Department'
+                placeholder='Select a department'
+                value={employee.companyDepartment}
+                onChange={handleCompanyDepartmentChange}
+                options={DEPARTMENTS}
               />
             </Form.Group>
-            <Form.Group controlId='zipAddress' className='mb-3'>
-              <Form.Label>Zip Code</Form.Label>
-              <Form.Control
-                type='number'
-                value={employee.zipAddress}
-                onChange={handleZipAddressChange}
+            <Form.Group as={Col} controlId='startDate' className='mb-3'>
+              <Form.Label>Start Date</Form.Label>
+              <DatePicker
+                placeholderText='Click to select a date'
+                dateFormat='yyyy/MM/dd'
+                autoComplete='off'
+                showMonthDropdown
+                showYearDropdown
+                todayButton={<Button>Today</Button>}
+                popperPlacement='bottom-start'
+                showPopperArrow={false}
+                showIcon
+                calendarIconClassName='end-0'
+                wrapperClassName='container g-0'
+                customInput={<Form.Control />}
+                selected={employee.startDate}
+                onChange={(date) =>
+                  setEmployee({
+                    ...employee,
+                    startDate: date.getTime()
+                  })
+                }
               />
             </Form.Group>
-          </fieldset>
-          <Form.Group controlId='companyDepartment' className='mb-3'>
-            <Form.Label>Department</Form.Label>
-            <SimpleSelect
-              as={Form.Select}
-              aria-label='Company Department'
-              placeholder='Select a department'
-              value={employee.companyDepartment}
-              onChange={handleCompanyDepartmentChange}
-              options={DEPARTMENTS}
-            />
-          </Form.Group>
+          </Row>
 
           <div className='d-flex justify-content-center'>
             <Button variant='primary' type='submit' className='w-100 mt-3'>
-              Save
+              Add employee to the database
             </Button>
           </div>
         </Form>
-      </Container>
+      </main>
 
       <Modal show={modal} onHide={hideModal}>
         <Modal.Header closeButton>
-          <Modal.Title>New employee created</Modal.Title>
+          <Modal.Title>New employee added to the database</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <pre className='border border-danger m-3 p-3'>
-            <h3 className='text-danger'>Form Data</h3>
-            {JSON.stringify(employee, null, 2)}
-          </pre>
+          <div className='d-block border-bottom p-2'>Name</div>
+          <p className='p-3'>
+            {employee.firstName} {employee.lastName}
+          </p>
+          <div className='d-block border-bottom p-2'>Birth Date</div>
+          <p className='p-3'>
+            {new Date(employee.birthDate).toLocaleDateString()}
+          </p>
+          <div className='d-block border-bottom p-2'>Address</div>
+          <p className='p-3'>
+            {employee.streetAddress}
+            <br />
+            {employee.cityAddress}, {employee.stateAddress}{' '}
+            {employee.zipAddress}
+          </p>
+          <div className='d-block border-bottom p-2'>Company Credentials</div>
+          <p className='p-3'>
+            Working in {employee.companyDepartment} {' since '}
+            {new Date(employee.startDate).toLocaleDateString()}
+          </p>
         </Modal.Body>
-        <Modal.Footer>
+        <Modal.Footer className='d-flex justify-content-between'>
+          <Button variant='secondary' onClick={goToList}>
+            Go to employees list
+          </Button>
           <Button variant='primary' onClick={hideModal}>
             Close
           </Button>
@@ -236,6 +265,14 @@ export function AddEmployee({ currentEmployee }) {
     setModal(true)
   }
   function hideModal() {
+    // Hide modal and reset HTML form
     setModal(false)
+    document.getElementById('addEmployeeForm').reset()
+    // Clear React controlled form by resetting the employee state back to newEmployee
+    setEmployee(newEmployee)
+  }
+  function goToList() {
+    setModal(false)
+    navigate('/')
   }
 }
